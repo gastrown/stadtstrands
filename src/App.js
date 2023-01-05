87eaeb728b48aa80546671e20acd0df7b2fdf22e
emmanuelmezie <irozuruchimezerem@gmail.com>
2023-01-05 09:47:12 +0100

    Files from ecr

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import AllRoutes from "./allRoutes";
 import dotenv from "dotenv";
-import Unzer from '@unzer/nodejs-sdk';
+// import Unzer from '@unzer/nodejs-sdk';
 
  // Creating an unzer instance with your public key
- var unzerInstance = new Unzer('s-pub-2a10156OrsiAKsbGpexvWY1XzeVwhsBj');
+//  var unzerInstance = new Unzer('s-pub-2a10156OrsiAKsbGpexvWY1XzeVwhsBj');
 
 
 dotenv.config();
